5c6e6548f2aca6fe7b807006341a60f52159fca6
Ayodele David <dayodele89@outlook.com>
2022-05-03 12:27:11 +0100

    Homepage UI commit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,20 @@
+from webbrowser import get
+from blog.models import Post
+from pyexpat import model
 from tempfile import template
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,DetailView,CreateView
+
 # Create your views here.
 class IndexView(TemplateView):
     template_name='index.html'
+
+
+
+class PostDetailView(DetailView):
+    model=Post
+
+
+
+class PostListView(CreateView):
+    model=Post
