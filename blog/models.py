1512c4b7ab62670b56fd1779e4a9ec50c7a9232b
Ayodele David <dayodele89@outlook.com>
2022-05-04 11:51:39 +0100

    Updated Post Model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from distutils.command.upload import upload
 from operator import mod
+from turtle import title
 from django.db import models
 from psycopg2 import Timestamp
 from django.utils import timezone
@@ -11,7 +12,11 @@ class Post(models.Model):
     author=models.CharField(max_length=50)
     date_created=models.DateTimeField(auto_now_add=True)
     publish_date=models.DateTimeField(null=True,default=None)
-    image=models.ImageField(upload_to='post_thumbnail')
+    image=models.ImageField(upload_to='post_thumbnail',default=None,null=True)
+
+
+    def __str__(self) -> str:
+        return self.title
 
     def publish(self):
         self.publish_date=timezone.now()
