ec4937efe2dae16c73221c0da86139c6a88bfea0
Ayodele David <dayodele89@outlook.com>
2022-05-04 18:50:43 +0100

    Post Detail Page Created

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from blog.views import PostListView
+from blog.views import PostListView,PostDetailView
 
 
 urlpatterns=[
-path('all/',PostListView.as_view(),name='post-list')
-
+path('all/',PostListView.as_view(),name='post-list'),
+path('<int:pk>',PostDetailView.as_view(),name='post-detail')
 
 ]
