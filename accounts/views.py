150f7023eecdad3f538edad8881ed21e395f4e96
Ayodele David <dayodele89@outlook.com>
2022-05-11 14:02:26 +0100

    Implemented Custom Login Functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
-
+from django.urls import reverse
+from django.shortcuts import render,redirect
+from django.views.generic import FormView,View
+from django.contrib.auth import login,authenticate
 # Create your views here.
+class LoginView(View):
+
+
+    def get(self,request,**kwargs):
+
+        return render(request,'account_login.html')
+
+
+    def post(self,request,**kwargs):
+        email=request.POST['email']
+        password=request.POST['password']
+
+        user=authenticate(username=email,password=password)
+
+        if not user:
+            return render(request,'account_login.html',context={'login_fail':True})
+        
+        login(user)
+
+        return redirect(reverse('post-list'))
--- datobi_blog/urls.py
+++ datobi_blog/urls.py
@@ -18,11 +18,13 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth import views
+from accounts.views import LoginView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('post/',include('blog.urls')),
-    path('login/', views.LoginView.as_view(), name='login'),
+    path('login/', LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout')
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
