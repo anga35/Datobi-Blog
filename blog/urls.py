9473c2a031c64963d73cba2e47edebb87c56dbad
Ayodele David <dayodele89@outlook.com>
2022-05-16 11:12:20 +0100

    Region name\

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from blog.views import PostListView,PostDetailView,PostCreateView,PostDraftsView
 
 
 urlpatterns=[
-path('all/',PostListView.as_view(),name='post-list'),
+
 path('<int:pk>',PostDetailView.as_view(),name='post-detail'),
 path('create/',PostCreateView.as_view(),name='post-create'),
 path('drafts/',PostDraftsView.as_view(),name='post-drafts'),
